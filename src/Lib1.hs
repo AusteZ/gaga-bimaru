b758445d77f8a37a84019b26fdc59b60272054b9
AusteZ <102924603+AusteZ@users.noreply.github.com>
2022-11-23 20:48:11 +0200

    Add files via upload (#2)

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Lib1(
-    State(..), emptyState, gameStart, render, mkCheck, toggle, hint, mergesort
+    State(..), emptyState, gameStart, render, mkCheck, toggle, hint
 ) where
 
 import Types
@@ -10,141 +10,36 @@ import Types
 -- number of occupied rows/cols, hints, occupied cells,..
 -- You can change the right hand side as you wish but please
 -- keep the type name as is
-
-data State = State {
-    guess :: [Coord],
-    ocRows :: [Int],
-    ocCols :: [Int],
-    hints :: Int,
-    hintCoords :: [Coord]
-    }
-    deriving (Show, Eq)
+data State = State [String]
+    deriving Show
 
 -- IMPLEMENT
 -- This is very initial state of your program
 emptyState :: State
-emptyState = State {
-    guess = [],
-    ocRows = [],
-    ocCols = [],
-    hints = 0,
-    hintCoords = []
-}
+emptyState = State ["Initial state"]
 
 -- IMPLEMENT
 -- This adds game data to initial state 
 gameStart :: State -> Document -> State
-gameStart _ (DMap (x1:x2:x3:_)) = State {
-    hints = parseDI (snd x1),
-    hintCoords = [],
-    ocCols = traverseList x2,
-    ocRows = traverseList x3,
-    guess = []
-} where
-    parseDI (DInteger a) = a
-    parseDI _ = -1
-
-    traverseList :: (String,Document) -> [Int]
-    traverseList (_, DMap (y1:y2:_)) = traverseList y1 ++ traverseList y2
-    traverseList (_ , DInteger i) = [i]
-    traverseList (_, _) = []
-gameStart _ _ = emptyState
+gameStart (State l) d = State $ ("Game started: " ++ show d) : l
 
 -- IMPLEMENT
--- renders yougame board
+-- renders your game board
 render :: State -> String
---render = show
-render (State g oR oC _ hC) = "     " ++ colAdder oC ++ rowAdder g oR 0
-    where
-        colAdder :: [Int] -> String
-        colAdder [] = "\n     " ++ showChs 'A'
-        colAdder (oCx:oCxs) = show oCx  ++ " " ++ colAdder oCxs
+render = show
 
-        showChs :: Char -> String
-        showChs 'K' = []
-        showChs ch = ch : " " ++ showChs (succ ch)
-
-        rowAdder :: [Coord] -> [Int] -> Int -> String 
-        rowAdder gx (oRx:oRxs) n 
-            | n == 10 = ""
-            | otherwise = "\n" ++ show oRx ++ "  " ++ show n ++ fillRow n 0 gx hC ++ rowAdder gx oRxs (n+1)
-        rowAdder _ _ _ = ""
-        fillRow :: Int -> Int -> [Coord] -> [Coord] -> String
-        fillRow n l (gx@(Coord gc gr):gs) (h@(Coord hc hr):hs)
-            | gr == hr && gc == hc = fillRow n l (gx:gs) hs
-            | gr == n && (hr > gr || (hr == gr && hc > gc)) = guessAdder l gx "x" ++ fillRow n (gc+1) gs (h:hs)
-            | hr == n && (hr < gr || (hr == gr && hc < gc)) = guessAdder l h "o" ++ fillRow n (hc+1) (gx:gs) hs
-            | gr < n = fillRow n l gs (h:hs)
-            | hr < n = fillRow n l (gx:gs) hs
-            | otherwise = cycleEmpty (10-l)
-        fillRow n l (gx@(Coord gc gr):gs) [] 
-            | n == gr = guessAdder l gx "x" ++ fillRow n (gc+1) gs []
-            | n > gr = fillRow n l gs [] 
-            | otherwise = cycleEmpty (10-l)
-        fillRow n l [] (h@(Coord hc hr):hs)
-            | n == hr = guessAdder l h "o" ++ fillRow n (hc+1) [] hs
-            | n > hr = fillRow n l [] hs
-            | otherwise = cycleEmpty (10-l)
-        fillRow _ l [] [] = cycleEmpty (10-l)
-        guessAdder :: Int -> Coord -> String -> String
-        guessAdder l (Coord c _) str = cycleEmpty (c-l) ++ str
-        cycleEmpty:: Int -> String
-        cycleEmpty i 
-            | i <= 0 = " "
-            | otherwise = " -" ++ cycleEmpty (i-1)
 -- IMPLEMENT
 -- Make check from current state
 mkCheck :: State -> Check
-mkCheck (State g _ _ _ _) = Check{coords = g}
+mkCheck _ = Check []
 
 -- IMPLEMENT
 -- Toggle state's value
 -- Receive raw user input tokens
 toggle :: State -> [String] -> State
-toggle (State g oR oC h hC) strs = State{
-    guess = mergesort (g ++ addCoord strs),
-    ocRows = oR,
-    ocCols = oC,
-    hints = h,
-    hintCoords = hC
-} where
-    addCoord :: [String] -> [Coord]
-    addCoord ((a:b:_):strxs)  = Coord (addBoth 'A' 0 a) (addBoth '0' 0 b) : addCoord strxs
-    addCoord _ = []
-    addBoth:: Char -> Int -> Char -> Int
-    addBoth l i ch = if l == ch then i else addBoth (succ l) (i+1) ch
+toggle (State l) t = State $ ("Toggle " ++ show t) : l
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
-hint (State g oR oC h _) (DMap ((_,DList x):_)) = State{
-    guess = g,
-    ocRows = oR,
-    ocCols = oC,
-    hints = h,
-    hintCoords = mergesort (traverseList x)
-} where
-    traverseList :: [Document] -> [Coord]
-    traverseList ((DMap (x1:x2:_)):xs) = Coord (getInt x1) (getInt x2) : traverseList xs
-    traverseList _ = []
-    getInt :: (String, Document) -> Int
-    getInt (_,DInteger xx) = xx
-    getInt _ = -1
-hint s _ = s
-
-mergesort'splitinhalf :: [Coord] -> ([Coord], [Coord])
-mergesort'splitinhalf cs = (take n cs, drop n cs)
-    where n = div (length cs) 2 
-mergesort'merge :: [Coord] -> [Coord] -> [Coord]
-mergesort'merge [] cs = cs
-mergesort'merge cs [] = cs
-mergesort'merge (x@(Coord cx rx):xs) (y@(Coord cy ry):ys)
-    | rx < ry || (rx == ry && cx < cy) = x : mergesort'merge xs (y:ys)
-    | rx == ry && cx == cy = mergesort'merge xs ys
-    | rx > 9 || cx > 9  = mergesort'merge xs (y:ys)
-    | otherwise = y : mergesort'merge (x:xs) ys
-mergesort :: [Coord] -> [Coord]
-mergesort cs
-    | length cs > 1 = mergesort'merge (mergesort ls) (mergesort rs)
-    | otherwise = cs
-    where (ls, rs) = mergesort'splitinhalf cs
+hint (State l) h = State $ ("Hint " ++ show h) : l
--- src/Lib2.hs
+++ src/Lib2.hs
@@ -2,110 +2,25 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lib2(renderDocument, hint, gameStart) where
 
-import Lib1 (State(..), mergesort)
-import Types
-
+import Types ( ToDocument(..), Document, Check )
+import Lib1 (State(..))
 
 -- IMPLEMENT
 -- First, make Check an instance of ToDocument class
 
-instance ToDocument Check where
-    toDocument (Check x) = DMap [("coords", DList (parseList x))]
-        where
-        parseList :: [Coord] -> [Document]
-        parseList ((Coord z y):cs) = (DMap [("col", DInteger z),("row", DInteger y)]) : parseList cs
-        parseList [] = []
-
 -- IMPLEMENT
 -- Renders document to yaml
 renderDocument :: Document -> String
-renderDocument d = "---" ++ (formatYaml "\n" d) ++ "\n"
-
-checkcheck :: String -> String
-checkcheck check
-    | elem ':' check = "\n" ++ init (init check) ++ "- "
-    | otherwise = check ++ "- "
-
-formatYaml :: String -> Document -> String
-formatYaml che (DList []) = if elem ':' che then ": []" else che
-formatYaml che (DList ds) = formatList (checkcheck che) ds
-
-    where
-        formatList :: String -> [Document] -> String
-        formatList check (x:xs) = (if elem ':' check then ":" else "") ++ formatYaml (nextCheck check) x ++ formatList (nextCheck check) xs
-        formatList _ [] = ""
-        nextCheck :: String -> String
-        nextCheck check
-            | elem ':' check = "\n" ++ (take ((length (check))-5) (cycle " ")) ++ "- "
-            | otherwise = init (init check) ++ "- "
-formatYaml che (DMap []) = che ++ "[]"
-formatYaml che (DMap ds) = formatMap che ds
-    where
-        formatMap :: String -> [(String, Document)] -> String
-        formatMap _ [] = ""
-        formatMap check ((str,x):xs) = checkMapCheck check ++ str ++ formatYaml ((if elem ':' check then "" else ":")++(nextCheck check) ++ "  ") x ++ formatMap (nextCheck check) xs
-        checkMapCheck :: String -> String
-        checkMapCheck check = if elem ':' check then init (init check) ++ "- " else check
-        nextCheck :: String -> String
-        nextCheck check
-            | elem '-' check = "\n" ++ (take ((length (check))-1) (cycle " "))
-            | otherwise = check
-formatYaml che (DString "") = (if (elem ':' che) then ":" else che) ++ ""
-formatYaml che (DString str) = (if (elem ':' che) then ": " else che) ++ "\"" ++ str ++ "\""
-formatYaml che (DInteger num) = (if (elem ':' che) then ": " else che) ++ (show num)
-formatYaml che DNull = (if (elem ':' che) then ": " else che) ++ "~"
+renderDocument _ = error "Implement me"
 
 -- IMPLEMENT
 -- This adds game data to initial state
 -- Errors are reported via Either but not error 
 gameStart :: State -> Document -> Either String State
-gameStart (State g _ _ _ hC) (DMap (z1:z2:z3:_)) = do
-    oR <- validate "occupied_rows" z3
-    oC <- validate "occupied_cols" z2
-    h <- hintparse z1
-    return (State g oR oC h hC)
-    where
-        validate :: String -> (String,Document) -> Either String [Int]
-        validate check (seccheck, DMap(x1:x2:_)) = if check == seccheck
-            then traverseList 10 x1 x2
-            else Left ("The input from server is not labeled as expected (" ++ check ++ ")")
-        validate _ _ = Left "Unexpected input in DMap (a DInteger and second another DMap or DNull)"
-        hintparse :: (String, Document) -> Either String Int
-        hintparse (check, DInteger num) = if check == "number_of_hints"
-            then Right num else Left "The input from server is not labeled as expected (number_of_hints)"
-        hintparse _ = Left "Error occured while parsing input from the server in gameStart"
+gameStart (State l) d = Right $ State $ ("Game started: " ++ show d) : l
 
-        traverseList :: Int -> (String,Document) -> (String,Document) -> Either String [Int]
-        traverseList nr (_, DInteger num) (_, DMap(x1:x2:_)) = do
-            let list = traverseList (nr-1) x1 x2
-            case list of
-                Right a -> return (num : a)
-                Left b -> Left b
-        traverseList nr (_, DInteger num) (_, DNull) = if (nr-1) == 0 then Right [num]
-        else Left ("The number of coordinates passed was too " ++ if nr < 0 then "high" else "low")
-        traverseList _ _ _ = Left "Unexpected input in DMap (a DInteger and second another DMap or DNull)"
-gameStart _ _ = Left "Error occured while parsing input from the server in gameStart"
 -- IMPLEMENT
 -- Adds hint data to the game state
 -- Errors are reported via Either but not error 
 hint :: State -> Document -> Either String State
---hint (State l) h = Right $ State $ ("Hint " ++ show h) : l
--- hint s d = Right s
-hint (State g oR oC h _) (DMap ((_, DList ds):_)) = do
-    hC <- traverseList ds
-    return (State g oR oC h hC)
-    where
-        traverseList :: [Document] -> Either String [Coord]
-        traverseList ((DMap ((_, DInteger num1):(_, DInteger num2):_)):xs) = do
-            let list = traverseList xs
-            case list of
-                Right a -> return (mergesort((Coord num1 num2) : a))
-                Left b -> Left b
-        traverseList [] = Right []
-        traverseList _ = Left "Error occured while parsing input from the server in hint"
-hint _ _ = Left "Error occured while parsing input from the server in hint"
-
-
-
-
-
+hint (State l) h = Right $ State $ ("Hint " ++ show h) : l
